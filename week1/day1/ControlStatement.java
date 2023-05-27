ac6b2b8af71b0acb4ed7fc765417a053875d0cee
PapithaP <132286158+PapithaP@users.noreply.github.com>
2023-05-27 21:23:46 +0530

    Add files via upload
    Week1

---
+++ week1/day1/ControlStatement.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class ControlStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i=0;i<=20;i++)
+		if 
+			(i%2==1) {
+			System.out.println("The given number"+" "+i+" "+"is odd");
+		}else {
+			System.out.println("The given number"+" "+i+" "+"is even");
+		}
+
+	}
+
+}
---
+++ week1/day1/Fruits.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Fruits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int kg = 2;
+		boolean ispresent = true;
+		String name = "Apple";
+		System.out.println(ispresent);
+		System.out.println("I want"+" " +kg+"kg"+" "+name);
+		float kgs = 3.5f;
+		String name1 = "JackFruit";
+		boolean available = false;
+		System.out.println(available);
+		System.out.println(name1+ " " + "is not available");
+		
+		
+	}
+
+}
